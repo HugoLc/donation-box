a5f71c45ee105fb072c7f6399372aab7abd22af9
Hugo LC <hugolindoso.c@gmail.com>
2021-10-15 18:56:13 -0300

    Criada a estrutura html primaria

---
+++ script.js
@@ -0,0 +1,14 @@
+
+(function() {
+
+    function function1() {}
+    function function2() {}
+
+    function init() {
+        console.log('iniciou o Javascript')
+        function1()
+        function2()
+    }
+
+    init()
+})()
